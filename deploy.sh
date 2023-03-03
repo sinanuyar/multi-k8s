7e30463f0ba4c2d0572c9e27d3d02dd2c0425b7c
sinanuyar <ozguruyar@gmail.com>
2023-03-03 23:29:04 +0000

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t sinanuyar/multi-client:latest -t sinanuyar/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t sinanuyar/multi-server:latest -t sinanuyar/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t sinanuyar/multi-worker:latest -t sinanuyar/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push sinanuyar/multi-client:latest
+docker push sinanuyar/multi-client:latest
+docker push sinanuyar/multi-client:latest
+
+docker push sinanuyar/multi-client:$SHA
+docker push sinanuyar/multi-client:$SHA
+docker push sinanuyar/multi-client:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=sinanuyar/multi-server:$SHA
+kubectl set image deployments/client-deployment client=sinanuyar/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=sinanuyar/multi-worker:$SHA
